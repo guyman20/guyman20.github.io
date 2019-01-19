2bcb4d2036642f1cd5608dbb89f0b1853d96902c
Guy Manzur <guyman20@gmail.com>
2019-01-19 22:35:56 +0200

    s

---
+++ js/length_index.js
@@ -0,0 +1,51 @@
+
+document.getElementById('words').addEventListener('input', myWordInput);
+document.getElementById('seconds').addEventListener('input', mySecondInput);
+
+function myWordInput(e){
+  words = document.getElementById('words').value;
+  if (!(/^\d+$/.test(words))) {
+    document.getElementById('words').value =
+      words.substring(0, words.length - 1);
+  }
+  
+	let temp = document.getElementById('seconds');
+  let num = (e.target.value/2.4).toFixed(1);
+	temp.value = isNaN(num) ? 0 : num;
+  
+  sendData();
+};
+
+function mySecondInput(e){
+  seconds = document.getElementById('seconds').value;
+  if (!(/^\d+$/.test(seconds))) {
+    document.getElementById('seconds').value =
+      seconds.substring(0, seconds.length - 1);
+  }
+  
+	let temp = document.getElementById('words');
+  let num = Math.round(e.target.value*2.4);
+  temp.value = isNaN(num) ? 0 : num;
+  
+  sendData();
+};
+
+
+function callback() {
+    var result = {};
+    result.valid = document.getElementById('seconds') > 0;
+    result.value = document.getElementById('seconds').toString();
+    console.log("sending submit");
+
+    JFCustomWidget.sendSubmit(result);
+}
+
+function sendData(){
+        var result = {};
+        result.valid = document.getElementById('seconds') > 0;
+        result.value = document.getElementById('seconds').toString();
+        JFCustomWidget.sendData(result);
+        console.log("sending data");
+}
+
+JFCustomWidget.subscribe("submit", callback);
